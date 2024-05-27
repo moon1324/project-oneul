e128b8a758c24ce040191d8637f34f71e6085632
seonah.developer <salj3895.developer@gmail.com>
2024-05-27 22:53:51 +0900

    desing: 메인페이지 레이아웃 추가

--- client/src/pages/banner/Banner.js
+++ client/src/pages/banner/Banner.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React from 'react';
 
 const Banner = () => {
-    return <div>배너</div>;
+    return (
+        <div>
+            <img src={process.env.PUBLIC_URL + '/images/banner/banner1.svg'}/>
+        </div>
+    );
 };
 
 export default Banner;
