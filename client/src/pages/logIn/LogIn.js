3020849b318172fd1a101f895bbd5e65ab45f318
moon1324 <michaelshouldhope@gmail.com>
2024-05-28 23:54:36 +0900

    temp: temp update login codes

--- client/src/pages/logIn/LogIn.js
+++ client/src/pages/logIn/LogIn.js
@@ -5,11 +5,21 @@ const LogIn = () => {
     return (
         <S.Background>
             <S.Wrapper>
-                <S.Twincle>
-                    <div className="logo-wrapper"></div>
+                <div
+                    className="logo-wrapper"
+                    style={{ width: "120px", height: "90px", display: "flex", alignItems: "center", justifyContent: "center", position: "relative" }}
+                >
+                    <div className="oneul" style={{ width: "82px", height: "64px" }}>
+                        <img src={process.env.PUBLIC_URL + "global/images/oneul.png"} style={{ width: "100%" }} />
+                    </div>
+                    <div className="cloud-moon" style={{ width: "48px", height: "36px", position: "absolute", left: "" }}>
+                        <img src={process.env.PUBLIC_URL + "global/images/cloud_moon.png"} style={{ width: "100%" }} />
+                    </div>
+                </div>
+                {/* <S.Twincle>
                     <div className="login-form"></div>
                     <div className="to-SignUp"></div>
-                </S.Twincle>
+                </S.Twincle> */}
             </S.Wrapper>
         </S.Background>
     );
--- client/src/pages/logIn/style.js
+++ client/src/pages/logIn/style.js
@@ -14,19 +14,16 @@ S.Background = styled.div`
 S.Wrapper = styled.div`
     width: 360px;
     height: 640px;
-    background-image: linear-gradient(
-        to bottom,
-        ${theme.PALETTE.skyblue},
-        ${theme.PALETTE.pink}
-    );
-    ${flexCenterColumn}
+    background-image: url(${process.env.PUBLIC_URL}/global/images/background.png);
+    display: flex;
+    flex-direction: column;
 `;
 
-S.Twincle = styled.div`
-    background-image: url(${process.env.PUBLIC_URL}/global/images/twincle.png);
-    width: 100%;
-    height: 60%;
-    ${flexCenterColumn}
-`;
+// S.Twincle = styled.div`
+//     background-image: url(${process.env.PUBLIC_URL}/global/images/twincle.png);
+//     width: 100%;
+//     height: 60%;
+//     ${flexCenterColumn}
+// `;
 
 export default S;
