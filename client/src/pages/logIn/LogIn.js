10e9d0b828ffe31d5ce21ebf06feb33b7fadbae7
moon1324 <michaelshouldhope@gmail.com>
2024-05-29 23:28:06 +0900

    design: end styling LogIn.js, css set

--- client/src/pages/logIn/LogIn.js
+++ client/src/pages/logIn/LogIn.js
@@ -1,25 +1,38 @@
 import React from "react";
 import S from "./style";
+import { Link } from "react-router-dom";
+import OneulInput from "../../components/input/OneulInput";
+import OneulButton from "../../components/button/OneulButton";
 
 const LogIn = () => {
     return (
         <S.Background>
             <S.Wrapper>
-                <div
-                    className="logo-wrapper"
-                    style={{ width: "120px", height: "90px", display: "flex", alignItems: "center", justifyContent: "center", position: "relative" }}
-                >
-                    <div className="oneul" style={{ width: "82px", height: "64px" }}>
-                        <img src={process.env.PUBLIC_URL + "global/images/oneul.png"} style={{ width: "100%" }} />
-                    </div>
-                    <div className="cloud-moon" style={{ width: "48px", height: "36px", position: "absolute", left: "" }}>
-                        <img src={process.env.PUBLIC_URL + "global/images/cloud_moon.png"} style={{ width: "100%" }} />
-                    </div>
-                </div>
-                {/* <S.Twincle>
-                    <div className="login-form"></div>
-                    <div className="to-SignUp"></div>
-                </S.Twincle> */}
+                <S.LogoWrapper>
+                    <Link to={"/logIn"}>
+                        <img src={process.env.PUBLIC_URL + "global/images/logo.png"} />
+                    </Link>
+                </S.LogoWrapper>
+                <S.LoginForm>
+                    <S.LoginLabel>
+                        <p>이메일</p>
+                        <OneulInput variant={"active"} size={"default"} />
+                    </S.LoginLabel>
+                    <S.LoginLabel>
+                        <p>비밀번호</p>
+                        <OneulInput variant={"active"} size={"default"} />
+                    </S.LoginLabel>
+                    <OneulButton variant={"skyblue"} border={"default"} size={"large"} color={"white"}>
+                        로그인
+                    </OneulButton>
+                </S.LoginForm>
+                <S.ToSignUp>
+                    <Link to={"/signUp"}>
+                        <OneulButton variant={"indigo"} border={"default"} size={"large"} color={"white"}>
+                            회원가입
+                        </OneulButton>
+                    </Link>
+                </S.ToSignUp>
             </S.Wrapper>
         </S.Background>
     );
--- client/src/pages/logIn/style.js
+++ client/src/pages/logIn/style.js
@@ -15,15 +15,43 @@ S.Wrapper = styled.div`
     width: 360px;
     height: 640px;
     background-image: url(${process.env.PUBLIC_URL}/global/images/background.png);
-    display: flex;
-    flex-direction: column;
+    ${flexCenterColumn}
 `;
 
-// S.Twincle = styled.div`
-//     background-image: url(${process.env.PUBLIC_URL}/global/images/twincle.png);
-//     width: 100%;
-//     height: 60%;
-//     ${flexCenterColumn}
-// `;
+S.LogoWrapper = styled.div`
+    width: 120px;
+    height: 90px;
+    ${flexCenter}
+    margin-top: 60px;
+    margin-bottom: 82px;
+    margin-left: 34px;
+    & img {
+        width: 100%;
+    }
+`;
+
+S.LoginForm = styled.form`
+    width: 100%;
+    height: 274px;
+    ${flexCenterColumn}
+    /* margin between password input and login button */
+    & button {
+        margin-top: 34px;
+    }
+`;
+
+S.LoginLabel = styled.label`
+    margin-bottom: 22px;
+    & p {
+        margin-left: 4px;
+        margin-bottom: 4px;
+        color: ${theme.PALETTE.white};
+        font-weight: 100;
+    }
+`;
+
+S.ToSignUp = styled.div`
+    margin: 56px 50px;
+`;
 
 export default S;
