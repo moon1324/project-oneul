4e61235c499e42c8c96f9579ddc5c859bbf84652
moon1324 <michaelshouldhope@gmail.com>
2024-06-04 17:24:32 +0900

    fix: fix router conflict

--- client/src/routes/router.js
+++ client/src/routes/router.js
@@ -21,6 +21,13 @@ import Banner3 from "../pages/banner/Banner3";
 import Banner4 from "../pages/banner/Banner4";
 import Banner5 from "../pages/banner/Banner5";
 import BannerMain from "../pages/banner/BannerMain";
+import SignUpMain from "../pages/signUp/SignUpMain";
+import SignUpStep1 from "../pages/signUp/SignUpStep1";
+import SignUpStep2 from "../pages/signUp/SignUpStep2";
+import SignUpStep3 from "../pages/signUp/SignUpStep3";
+import SignUpStep4 from "../pages/signUp/SignUpStep4";
+import SignUpStep5 from "../pages/signUp/SignUpStep5";
+import SignUpSuccess from "../pages/signUp/SignUpSuccess";
 
 const router = createBrowserRouter([
     {
@@ -50,29 +57,28 @@ const router = createBrowserRouter([
             {
                 path: "/myPage",
                 element: <MyPage />,
-                children:[
+                children: [
                     {
-                        path:"/myPage",
-                        element:<MyPageMain/>
+                        path: "/myPage",
+                        element: <MyPageMain />,
                     },
                     {
-                        path:"/myPage/edit",
-                        element:<MyPageEdit/>
+                        path: "/myPage/edit",
+                        element: <MyPageEdit />,
                     },
                     {
-                        path:"/myPage/termsOfUse",
-                        element:<TermsOfUse/>
+                        path: "/myPage/termsOfUse",
+                        element: <TermsOfUse />,
                     },
                     {
-                        path:"/myPage/privacyPolicy",
-                        element:<PrivacyPolicy/>
+                        path: "/myPage/privacyPolicy",
+                        element: <PrivacyPolicy />,
                     },
                     {
-                        path:"/myPage/secession",
-                        element:<Secession/>
-
-                    }
-                ]
+                        path: "/myPage/secession",
+                        element: <Secession />,
+                    },
+                ],
             },
         ],
     },
@@ -83,36 +89,66 @@ const router = createBrowserRouter([
     {
         path: "/signUp",
         element: <SignUp />,
+        children: [
+            {
+                path: "/signUp",
+                element: <SignUpMain />,
+            },
+            {
+                path: "/signUp/1",
+                element: <SignUpStep1 />,
+            },
+            {
+                path: "/signUp/2",
+                element: <SignUpStep2 />,
+            },
+            {
+                path: "/signUp/3",
+                element: <SignUpStep3 />,
+            },
+            {
+                path: "/signUp/4",
+                element: <SignUpStep4 />,
+            },
+            {
+                path: "/signUp/5",
+                element: <SignUpStep5 />,
+            },
+            {
+                path: "/signUp/success",
+                element: <SignUpSuccess />,
+            },
+        ],
     },
     {
         path: "/banner",
         element: <Banner />,
-        children : [
+        children: [
             {
-                path:"/banner",
-                element : <BannerMain/>
-            }
-            ,{
-               path:"/banner/banner1",
-               element : <Banner1/> 
+                path: "/banner",
+                element: <BannerMain />,
             },
             {
-                path:"/banner/banner2",
-                element : <Banner2/> 
-             },
-             {
-                path:"/banner/banner3",
-                element : <Banner3/> 
-             },
-             {
-                path:"/banner/banner4",
-                element : <Banner4/> 
-             },
-             {
-                path:"/banner/banner5",
-                element : <Banner5/> 
-             }
-        ]
+                path: "/banner/banner1",
+                element: <Banner1 />,
+            },
+            {
+                path: "/banner/banner2",
+                element: <Banner2 />,
+            },
+            {
+                path: "/banner/banner3",
+                element: <Banner3 />,
+            },
+            {
+                path: "/banner/banner4",
+                element: <Banner4 />,
+            },
+            {
+                path: "/banner/banner5",
+                element: <Banner5 />,
+            },
+        ],
     },
     {
         path: "*",
