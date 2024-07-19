f7df357adaa1323d1466fd4f9c9037c3b7114606
moon1324 <michaelshouldhope@gmail.com>
2024-07-19 15:51:45 +0900

    feat: add search functionality

--- client/src/pages/search/Search.js
+++ client/src/pages/search/Search.js
@@ -1,18 +1,54 @@
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import S from "./style";
-import SearchResultPost from "./SearchResultPost";
+import SearchPosts from "./SearchPosts";
 import SearchResultMyMind from "./SearchResultMyMind";
-
+import { useLocation } from "react-router-dom";
 
 const Search = () => {
+    // const getPosts = async () => {
+    //     try {
+    //         console.log(searchValue);
+    //         const response = await fetch(`http://localhost:8000/search?value=${searchValue}`);
+    //         const data = await response.json();
+    //         console.log(data);
+    //         return data;
+    //     } catch (error) {
+    //         console.error("Failed to search", error);
+    //     }
+    // };
+
+    // getPosts().then(setPosts);
+    // console.log(posts);
+
+    // useEffect(() => {
+    //     console.log(getPosts());
+    //     getPosts().then(setPosts());
+    //     console.log(posts);
+    // }, []);
+
     return (
         <>
             <S.Wrapper>
+                {/* <S.header>
+                    <S.searchSection>우리의 오늘</S.searchSection>
+                    <S.viewAll>
+                        모두보기
+                        <FontAwesomeIcon icon={faAngleRight} />
+                    </S.viewAll>
+                </S.header> */}
+                <S.searchContainer>
+                    <SearchPosts />
+                </S.searchContainer>
+            </S.Wrapper>
+            {/* <S.Wrapper>
                 <S.header>
                     <S.searchSection>나의 마음보기</S.searchSection>
-                    <S.viewAll>모두보기<FontAwesomeIcon icon={faAngleRight} /></S.viewAll>
+                    <S.viewAll>
+                        모두보기
+                        <FontAwesomeIcon icon={faAngleRight} />
+                    </S.viewAll>
                 </S.header>
                 <S.searchContainer>
                     <SearchResultMyMind />
@@ -21,12 +57,15 @@ const Search = () => {
             <S.searchOurDayWrapper>
                 <S.header>
                     <S.searchSection>우리의 오늘</S.searchSection>
-                    <S.viewAll>모두보기<FontAwesomeIcon icon={faAngleRight} /></S.viewAll>
+                    <S.viewAll>
+                        모두보기
+                        <FontAwesomeIcon icon={faAngleRight} />
+                    </S.viewAll>
                 </S.header>
                 <S.searchContainer>
                     <SearchResultPost />
                 </S.searchContainer>
-            </S.searchOurDayWrapper>
+            </S.searchOurDayWrapper> */}
         </>
     );
 };
---
+++ server/controller/search/search.js
@@ -0,0 +1,30 @@
+import MyMind from "../../models/myMindSchema.js";
+import OurToday from "../../models/ourTodaySchema.js";
+
+const searchKeyword = async (req, res) => {
+    const { value } = req.query;
+    if (!value) {
+        return res.status(400).json({ message: "Search value is required" });
+    }
+
+    // const userEmail = req.user.email;
+    // console.log(userEmail);
+
+    try {
+        // const myMindResults = await MyMind.find({
+        //     userEmail: userEmail,
+        //     questions: { $regex: value, $options: "i" },
+        // });
+
+        const ourTodayResults = await OurToday.find({
+            content: { $regex: value, $options: "i" },
+        });
+
+        // res.json({ myMindResults, ourTodayResults });
+        res.json(ourTodayResults);
+    } catch (error) {
+        res.status(500).json({ message: "Failed to search", error });
+    }
+};
+
+export { searchKeyword };
